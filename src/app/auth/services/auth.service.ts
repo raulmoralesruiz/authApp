c885fa6476fc3278eb8f4e2ff0347f28c1269383
Raúl Morales <raulmdaw@gmail.com>
2024-02-08 19:52:16 +0100

    361. AuthService

--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -1,9 +1,13 @@
-import { Injectable } from '@angular/core';
+import { Injectable, inject } from '@angular/core';
+import { environment } from '../../../environments/environments';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+  private readonly baseUrl: string = environment.baseUrl;
+  private http = inject(HttpClient);
 
   constructor() { }
 }
